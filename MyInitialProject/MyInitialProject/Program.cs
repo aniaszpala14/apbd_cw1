1d56295ab1f69cb796171891ef31b42ab70d2bbe
s27542 <s27542@pjwstk.edu.pl>
2024-03-14 16:25:12 +0100

    Changed result to result2

--- MyInitialProject/MyInitialProject/Program.cs
+++ MyInitialProject/MyInitialProject/Program.cs
@@ -6,13 +6,13 @@ Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
 static double GetAverage(int[] numbers)
 {
-    double result = 0.0;
+    double result2 = 0.0;
     for (int i = 0; i < numbers.Length; i++)
     {
-        result += numbers[i];
+        result2 += numbers[i];
     }
 
-    return result / numbers.Length;
+    return result2 / numbers.Length;
 }
 static double GetMax(int[] numbers)
 {
